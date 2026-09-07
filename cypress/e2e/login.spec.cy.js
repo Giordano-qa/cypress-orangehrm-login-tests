e54d092aedb11c46a70c5c8d4e5350403e511155
devgbmc <devgbmc@gmail.com>
2026-09-07 16:39:42 -0300

    Refactor: centraliza seletores no selectors List

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,26 @@
 describe('Orange HRM Tests', () => {
+  const selectorsList = {
+  usernameField: "[name='username']",
+  passwordField: "[name='password']",
+  loginButton: "[type= 'submit']",
+  SectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+  wrongCredentialAlert: "[role='Alert']"
+  }
+ 
   it('login-success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorsList.SectionTitleTopBar).contains('Dashboard')
   })
 
   it('login-Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('input[name="username"]').type('test')
-    cy.get('input[name="password"]').type('test')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert-content-text').should('be.visible').and('contain', 'Invalid credentials')
+    cy.get(selectorsList.usernameField).type('test')
+    cy.get(selectorsList.passwordField).type('test')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   })
 })
