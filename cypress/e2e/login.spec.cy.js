b1ef3e9653c5c5f6e677686ea8bfbc1144617f1f
devgbmc <devgbmc@gmail.com>
2026-09-14 18:00:57 -0300

    Test: adiciona cenarios de sucesso e falha no login

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,26 +1,24 @@
 describe('Orange HRM Tests', () => {
   const selectorsList = {
-  usernameField: "[name='username']",
-  passwordField: "[name='password']",
-  loginButton: "[type= 'submit']",
-  SectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-  wrongCredentialAlert: "[role='Alert']"
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']"
   }
- 
-  it('login-success', () => {
+  it('Login - Sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorsList.usernameField).type('Admin')
     cy.get(selectorsList.passwordField).type('admin123')
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.SectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
-
-  it('login-Fail', () => {
+  it('Login - Falha', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorsList.usernameField).type('test')
     cy.get(selectorsList.passwordField).type('test')
     cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
+    cy.get(selectorsList.wrongCredentialAlert).should('be.visible')
   })
 })
